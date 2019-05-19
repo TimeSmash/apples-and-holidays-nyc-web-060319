056fb955921467b209637fec75d0a02be4783752
mcumm64 <mcumm64@gmail.com>
2019-05-19 23:54:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,9 +57,9 @@ def all_winter_holiday_supplies(holiday_hash)
       supply
     end
 
-# winter_supplies.flatten
+winter_supplies.flatten
 
-# end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
